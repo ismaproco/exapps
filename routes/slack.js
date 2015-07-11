fbefe977ee663a2a07741352715fbc27855c5566
Ismael Jimenez <ijimenezgarzon@gmail.com>
2015-07-11 17:58:08 -0600

    added router for the slack management

---
+++ routes/slack.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+var slack = require('../controllers/slackmn');
+
+/* manage POST request from the slackwebhook  */
+router.post('/', function(req, res, next) {
+  var message = slack.parseHeader( req.headers );
+  slack.saveMessage(message);
+  res.send( JSON.stringify( slack.loadMessages() ) );
+});
+
+router.get('/', function(req, res, next) {
+  res.send( '' );
+});
+
+
+module.exports = router;
